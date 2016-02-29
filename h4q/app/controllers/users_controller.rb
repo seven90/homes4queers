5e3d7b58f6a78f506e684fb322e85f469e51ac1f
Your Name <sylviapereira@gmail.com>
2016-02-29 17:25:08 -0500

    more yay

--- h4q/app/controllers/users_controller.rb
+++ h4q/app/controllers/users_controller.rb
@@ -45,6 +45,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation, :about_me, :image =>[:image])
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :about_me, :avatar)
   end
 end
