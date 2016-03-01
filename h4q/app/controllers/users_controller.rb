ed570a1fb768ab322525d11f78ee289281a397e1
stefanovtamara <stefanov.tamara@gmail.com>
2016-03-01 15:11:43 -0500

    redo user controller changes

--- h4q/app/controllers/users_controller.rb
+++ h4q/app/controllers/users_controller.rb
@@ -45,6 +45,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
+    #image is nested in a hash
     params.require(:user).permit(:name, :email, :password, :password_confirmation, :about_me, :avatar)
   end
 end
