36e83db91671f33a796a3382c030c985ae59a0ca
stefanovtamara <stefanov.tamara@gmail.com>
2016-03-04 00:22:29 -0500

    trying new things that don't work!

--- h4q/app/assets/javascripts/map.js
+++ h4q/app/assets/javascripts/map.js
@@ -1,29 +1,9 @@
-// function Map(mapId) {
-//   this.mapId = mapId;
-// }
-//
-// Map.prototype.init = function(latitude, longitude){
-//   var options = {
-//     center: {lat: latitude, long: longitude},
-//     zoom: 15,
-//     mapTypeId: google.maps.MapTypeId.ROADMAP
-//   };
-//
-//   this.canvas = new google.maps.Map(this.mapId, options);
-// }
-//
-// $(document).on('ready page:load', function(){
-//   if ($('#map-show').length) {
-//     window.myMap = new Map($('#map-show').get());
-//     window.myMap.init(43.6426, -79.3871);
-//   }
-// })
-//
+
 $(document).on('ready page:load', function(){
   var canvas = $('#map-show');
       listingLocation = {lat: canvas.data('latitude'), lng: canvas.data('longitude')}
 
-     window.initMap = function() {
+     initMap = function() {
       map = new google.maps.Map(canvas[0], {
         center: listingLocation,
         zoom: 12
@@ -33,6 +13,4 @@ $(document).on('ready page:load', function(){
         map: map
       });
     }
-
-
 });
---
+++ questionsFacebook.md
@@ -0,0 +1,16 @@
+Questions for Facebook users:
+
+- Do you want to Sign in through Facebook login or native login
+- Should we only invite users through a sign up code, or have it public
+- How do you search for apartments
+	Mobile 		Desktop
+- How do you use the H4Q facebook page
+	Mobile		Desktop
+- Rate how important these features are (1-4)
+* Chat
+* Censorship (automatically blocking potentially harmful language)
+* Uploading pictures from various sources (Facebook, URL, native hard drive)
+* Being able to bookmark listings
+
+- Are you willing to beta test for us?
+
