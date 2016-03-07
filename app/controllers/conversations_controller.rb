64ed597d6fa2b845347e32f02236c6fe7003a807
sofanisba <stefanov.tamara@gmail.com>
2016-03-07 16:33:57 -0500

    search through users to message

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -2,8 +2,13 @@ class ConversationsController < ApplicationController
   before_action :require_login
 
   def index
-    @users = User.all
     @conversations = Conversation.all
+    @users = if params[:search]
+      User.where("LOWER(name) LIKE LOWER(?)", "%#{params[:search]}%")
+    else
+      User.all
+    end
+
   end
 
   def create
