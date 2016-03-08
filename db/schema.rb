19ecd6d2d8347dadebbe72bdc40713776a07f147
Your Name <sylviapereira@gmail.com>
2016-03-08 12:25:31 -0500

    Commit db migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160307213603) do
+ActiveRecord::Schema.define(version: 20160308163220) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -118,6 +118,9 @@ ActiveRecord::Schema.define(version: 20160307213603) do
     t.integer  "failed_logins_count",          default: 0
     t.datetime "lock_expires_at"
     t.string   "unlock_token"
+    t.string   "token"
+    t.string   "invite_code"
+    t.text     "external_links"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
