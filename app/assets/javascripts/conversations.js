ba23cfcadc4a607c7d52b5e5b242ef269409acf6
sofanisba <stefanov.tamara@gmail.com>
2016-03-09 12:29:10 -0500

    refactor views to be prettier

--- app/assets/javascripts/conversations.js
+++ app/assets/javascripts/conversations.js
@@ -1,9 +1,12 @@
 // # Place all the behaviors and hooks related to the matching controller here.
 // # All this logic will automatically be available in application.js.
 // # You can use CoffeeScript in this file: http://coffeescript.org/
+//
 
-$(document).on('ready page:load', function(){
+//this code breaks my map :(
 
-document.getElementById("message_body").focus();
-
-})
+// $(document).on('ready page:load', function(){
+//
+// document.getElementById("message_body").focus();
+//
+// })
