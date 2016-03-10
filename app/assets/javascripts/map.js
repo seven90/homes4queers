54c8b78c85c78bb7623b85e488982b6bf8cdc4d2
sofanisba <stefanov.tamara@gmail.com>
2016-03-09 21:20:06 -0500

    making the map cute

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,40 +1,41 @@
 
 $(document).on('ready page:load', function(){
   var canvas = $('#map-show');
-      listingLocation = {lat: canvas.data('latitude'), lng: canvas.data('longitude')}
+  listingLocation = {lat: canvas.data('latitude'), lng: canvas.data('longitude')}
 
-     initMap = function() {
-      var map = new google.maps.Map(canvas[0], {
-        center: listingLocation,
-        zoom: 10
+  initMap = function() {
+    var map = new google.maps.Map(canvas[0], {
+      center: {lat: 43.7, lng: -79.4},
+      zoom: 10
+  });
+
+  var manyIcon = ('/pride_flag.gif')
+  var singleIcon = ('/google_pin.png')
+
+  var marker = new google.maps.Marker({
+    position: listingLocation,
+    map: map,
+    animation: google.maps.Animation.DROP,
+    icon: singleIcon
+  });
+
+//window.nearby used for show nearby listings and index all listings cause laziness or DRY
+  if (window.nearby) {
+    nearby.forEach(function(coord) {
+      new google.maps.Marker({
+        position: { lat: parseFloat(coord.lat), lng: parseFloat(coord.lng) },
+        map: map,
+        animation: google.maps.Animation.DROP,
+        icon: manyIcon
+
       });
+    });
+  }
 
-      // var nearbys = window.nearby
-      // for(i = 0; i< nearby; i++){
-      //   marker = new google.map.Marker({
-      //     position: nearbys
-      //     map:
-      //   });
-      // }
-
-      var marker = new google.maps.Marker({
-        position: listingLocation,
-        map: map
-      });
-
-    if (window.nearby) {
-      nearby.forEach(function(coord) {
-        new google.maps.Marker({
-          position: { lat: parseFloat(coord.lat), lng: parseFloat(coord.lng) },
-          map: map,
-
-        });
-      });
 }
-    }
 
-    if (canvas.length > 0){
-      initMap();
-    }
+  if (canvas.length > 0){
+    initMap();
+  }
 
 });
